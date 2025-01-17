684246ba619def4de1b7595b6b76f4b4e01fc8a3
giusmili <giusmili67@gmail.com>
2025-01-17 17:58:59 +0100

    first commit

---
+++ js/demo.js
@@ -0,0 +1,6 @@
+let contents = `Vous etes sur ${navigator.language}`;
+window.alert(contents); /* affichage BOM navigateir */
+/* console navigateur */
+console.log(contents);
+console.info(contents);
+console.error();
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Cours Javascript - ES6+ 🚀
