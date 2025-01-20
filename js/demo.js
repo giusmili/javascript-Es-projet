3fe0f4cdf7df447eb069d38a7236080d02e43ef5
giusmili <giusmili67@gmail.com>
2025-01-20 17:43:07 +0100

    js

--- js/demo.js
+++ js/demo.js
@@ -1,6 +1,11 @@
 let contents = `Vous etes sur ${navigator.language}`;
-//window.alert(contents); /* affichage BOM navigateir */
+window.alert(contents); /* affichage BOM navigateir */
 /* console navigateur */
 console.log(contents);
 console.info(contents);
-console.error();
+console.error(contents);
+const parent= document.querySelector("button");
+console.log(parent);
+parent.addEventListener("click", () => {
+    console.log("ok");
+});
--- src/demo.js
+++ src/demo.js
@@ -3,4 +3,9 @@ window.alert(contents)/* affichage BOM navigateir */
 /* console navigateur */
 console.log(contents)
 console.info(contents)
-console.error()
+console.error(contents)
+const parent = document.querySelector("button")
+console.log(parent)
+parent.addEventListener("click",()=>{
+    console.log("ok")
+})
