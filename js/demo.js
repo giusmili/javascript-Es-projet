53c9d44632a5069579ed35c518ee36f57d0da2da
giusmili <giusmili67@gmail.com>
2025-01-20 11:12:44 +0100

    js

--- js/demo.js
+++ js/demo.js
@@ -1,5 +1,5 @@
 let contents = `Vous etes sur ${navigator.language}`;
-window.alert(contents); /* affichage BOM navigateir */
+//window.alert(contents); /* affichage BOM navigateir */
 /* console navigateur */
 console.log(contents);
 console.info(contents);
