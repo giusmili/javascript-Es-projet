1de3b84f51ce8064cf10a07b258f3330088ab2f2
giusmili <giusmili67@gmail.com>
2025-01-21 18:25:02 +0100

    js

--- js/demo.js
+++ js/demo.js
@@ -1,6 +1,14 @@
 let contents = `Vous etes sur ${navigator.language}`;
-/* window.alert(contents); */ /* affichage BOM navigateir */
+//window.alert(contents)/* affichage BOM navigateir */
 /* console navigateur */
 console.log(contents);
 console.info(contents);
 console.error();
+
+const btn = document.querySelector("button");
+
+const links = ["https://www.wikipedia.org"];
+
+btn.addEventListener("click", () => {
+   window.open(links[0]); /* location.href="" */
+});
