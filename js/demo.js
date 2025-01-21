e96b6e762ba4dc5a39faded83888457830a9fcca
giusmili <giusmili67@gmail.com>
2025-01-21 18:33:32 +0100

    js

--- js/demo.js
+++ js/demo.js
@@ -1,14 +1,17 @@
-let contents = `Vous etes sur ${navigator.language}`;
-//window.alert(contents)/* affichage BOM navigateir */
-/* console navigateur */
-console.log(contents);
-console.info(contents);
-console.error();
+document.addEventListener("DOMContentLoaded", e => {
+    e.preventDefault();
+    let contents = `Vous etes sur ${navigator.language}`;
+    //window.alert(contents)/* affichage BOM navigateir */
+    /* console navigateur */
+    console.log(contents);
+    console.info(contents);
+    console.error();
 
-const btn = document.querySelector("button");
+    const btn = document.querySelector("button");
 
-const links = ["https://www.wikipedia.org"];
+    const links = ["https://www.wikipedia.org"];
 
-btn.addEventListener("click", () => {
-   window.open(links[0]); /* location.href="" */
+    btn.addEventListener("click", () => {
+        window.open(links[0]); /* location.href="" */
+    });
 });
